96edc1a16d4314e3aa56dad532ebe20c86a8e97d
unknown <37jr@naver.com>
2024-02-22 15:49:10 +0900

    spring boot 초기세팅

---
+++ src/test/java/crosstheline/wanted_backend/WantedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package crosstheline.wanted_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
